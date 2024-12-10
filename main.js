a8ac8e2c692dd59e61cabe2015899406230db88a
ddhodi <dhodisuryasayogo@gmail.com>
2024-12-10 22:49:35 +0700

    pemasangan komen button di js

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ paginationItems.forEach((item) => {
   });
 });
 
-// Handle Previous and Next buttons
+// Handle Previous and Next buttons pgination
 prevPage.addEventListener("click", (e) => {
   e.preventDefault();
   const activePage = document.querySelector(".pagination .active .page-link").dataset.page;
@@ -86,6 +86,7 @@ nextPage.addEventListener("click", (e) => {
   }
 });
 
+// button search
 document.addEventListener("DOMContentLoaded", () => {
   const searchButton = document.getElementById("searchButton");
   const searchForm = document.getElementById("searchForm");
